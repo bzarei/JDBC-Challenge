a09b0437ba44e272362b8e64700f16128c47544a
Baharak Zarei <baharak.zarei@telekom.de>
2021-05-18 18:45:53 +0200

    added DELETE and completed Exceptions

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -16,39 +16,51 @@ public class Main {
 		try (Connection connection = DriverManager
 				.getConnection("jdbc:mariadb://localhost:3306/seadb?user=seauser&password=seapass");) {
 
-/*			String delsql = "DELETE FROM personen where id in(1,2)";
-			PreparedStatement delps = connection.prepareStatement(delsql);
-*/
-			String insert = "INSERT INTO personen (ID, ANREDE, VORNAME, NACHNAME) VALUES ( ?, ?, ?, ? )";
-			try (PreparedStatement ps = connection.prepareStatement(insert);) {
-				ps.setLong(1, 1L);
-				ps.setShort(2, (short) 2);
-				ps.setString(3, "Baharak");
-				ps.setString(4, "Zarei");
-				ps.execute();
+			String query = "DELETE FROM personen WHERE id=1 OR id=2";
+			try (PreparedStatement delps = connection.prepareStatement(query);) {
+				delps.execute();
 
-				ps.setLong(1, 2L);
-				ps.setShort(2, (short) 1);
-				ps.setString(3, "Henrick");
-				ps.setString(4, "Mustermann");
-				ps.execute();
+				query = "INSERT INTO personen (ID, ANREDE, VORNAME, NACHNAME) VALUES ( ?, ?, ?, ? )";
+				try (PreparedStatement ps = connection.prepareStatement(query);) {
+					ps.setLong(1, 1L);
+					ps.setShort(2, (short) 2);
+					ps.setString(3, "Baharak");
+					ps.setString(4, "Zarei");
+					ps.execute();
 
-				String select = "SELECT * FROM personen";
-				try (Statement statement = connection.createStatement();) {
-					statement.executeQuery(select);
-					try (ResultSet resultset = statement.executeQuery(select);) {
-						System.out.println("+----+--------+---------+------------+");
-						System.out.println("| ID | ANREDE | VORNAME | NACHNAME   |");
-						System.out.println("+----+--------+---------+------------+");
-						while (resultset.next()) {
-							System.out.println("|  " + resultset.getLong(1) 
-								+ " |      " + resultset.getShort(2) 
-								+ " | " + resultset.getString(3)
-								+ " | " + resultset.getString(4));
-						}
-					} catch (Exception ex) { } // resultset.close();
-				} catch (Exception ex) { } // statement.close();
-			} catch (Exception ex) { } // statement.close();
-		} catch (Exception ex) { } // connection.close();
+					ps.setLong(1, 2L);
+					ps.setShort(2, (short) 1);
+					ps.setString(3, "Henrick");
+					ps.setString(4, "Mustermann");
+					ps.execute();
+
+					/**
+					 * create SELECT statement and execute it
+					 */
+					query = "SELECT * FROM personen";
+					try (Statement statement = connection.createStatement();) {
+						try (ResultSet resultset = statement.executeQuery(query);) {
+							System.out.println("+----+--------+---------+------------+");
+							System.out.println("| ID | ANREDE | VORNAME | NACHNAME   |");
+							System.out.println("+----+--------+---------+------------+");
+							while (resultset.next()) {
+								System.out.println("|  " + resultset.getLong(1) + " |      " + resultset.getShort(2)
+										+ " | " + resultset.getString(3) + " | " + resultset.getString(4));
+							}
+						} catch (Exception ex) {
+							System.out.println(" Fehler beim SELECT: executeQuery! ");
+						  } // resultset.close();
+					} catch (Exception ex) {
+						System.out.println(" Fehler beim SELECT: createStatement! ");
+					  } // statement.close();
+				} catch (Exception ex) {
+					System.out.println(" Fehler beim INSERT: prepareStatement(insert)! ");
+				  } // statement.close();
+			} catch (Exception ex) {
+				System.out.println(" Fehler beim DELETE: prepareStatement(delete)! ");
+			  } // statement.close();
+		} catch (Exception ex) {
+			System.out.println(" Fehler beim Connection! ");
+		  } // connection.close();
 	}
 } // Ende Klasse Main
